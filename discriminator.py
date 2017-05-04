3531d9fe6ce78d277d5c95c20e00f08763177f2e
mxl1990 <mxl1990@gmail.com>
2017-05-04 20:01:57 +0800

    1.拆分GAN模型中的G和D的代码 2.复用D的代码，去除原来代码中拼接复制参数的部分 3.学习数据本身，而不是学习均值和方差

---
+++ discriminator.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+from tensorflow.python.ops import variable_scope
+dim = 1000
+
+def Discrim(input_data):
+    reuse = len([t for t in tf.global_variables() if t.name.startswith('Discrim')]) > 0
+    with variable_scope.variable_scope('Discrim', reuse = reuse):
+        dWeights = tf.Variable(tf.random_normal([dim, 32]))
+        dbiases = tf.Variable(tf.zeros([1, 32]) + 0.1)
+        D_output = tf.matmul(input_data, dWeights) + dbiases
+        D_output = tf.nn.relu(D_output)
+        # 第二层
+        dWeights2 = tf.Variable(tf.random_normal([32, 32]))
+        dbiases2 = tf.Variable(tf.zeros([1, 32]) + 0.1)
+        D_output2 = tf.matmul(D_output, dWeights2) + dbiases2
+        D_output2 = tf.nn.sigmoid(D_output2)
+
+        # 第三层
+        dWeights3 = tf.Variable(tf.random_normal([32, 1]))
+        dbiases3 = tf.Variable(tf.zeros([1, 1]) + 0.1)
+        D_output3_ = tf.matmul(D_output2, dWeights3) + dbiases3
+        D_output3 = tf.nn.sigmoid(D_output3_)
+
+    # 返回一个原始输出和一个经过激励以后的输出
+    # 返回非激励用于计算Loss
+    # 计算loss的函数有要求不能在之前算激励
+    return D_output3_, D_output3
---
+++ generator.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+from tensorflow.python.ops import variable_scope
+dim = 1000
+
+def Gen(noise):
+	###################################### G  网络结构
+	# 第一层
+	# 每个噪声数据维度为LENGTH
+	reuse = len([t for t in tf.global_variables() if t.name.startswith('Generator')]) > 0
+	with variable_scope.variable_scope('Generator', reuse = reuse):
+	    # z = tf.placeholder(tf.float32, shape=[None, LENGTH])  # 随机值噪音
+	    # 随机权重
+	    Weights = tf.Variable(tf.random_normal([dim, 32]))
+	    # 偏差为0.1
+	    biases = tf.Variable(tf.zeros([1, 32]) + 0.1)
+	    # G_output = z * w + b
+	    G_output = tf.matmul(noise, Weights) + biases
+	    # Rectified Linear Units激活函数
+	    # relu(x) = max(0,x)即比0大就取本身
+	    G_output = tf.nn.relu(G_output)
+	    # 第二层
+	    Weights2 = tf.Variable(tf.random_normal([32, 32]))
+	    biases2 = tf.Variable(tf.zeros([1, 32]) + 0.1)
+	    G_output2 = tf.matmul(G_output, Weights2) + biases2
+	    # 第二层激活函数为sigmoid
+	    G_output2 = tf.nn.sigmoid(G_output2)
+	    # 第三层
+	    Weights3 = tf.Variable(tf.random_normal([32, dim]))
+	    biases3 = tf.Variable(tf.zeros([1, dim]) + 0.1)
+	    G_output3 = tf.matmul(G_output2, Weights3) + biases3
+
+	# G_PARAMS = [Weights, biases, Weights2, biases2, Weights3, biases3]  # G的参数
+	return G_output3
