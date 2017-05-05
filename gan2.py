5af7863b0b9e79884950d3d5d0077b117c3df73a
mxl1990 <mxl1990@gmail.com>
2017-05-05 18:02:46 +0800

    1.修改训练集的生成时间，一次性生成训练所用数据集 2.修改loss定义部分，但依旧存在问题

--- gan2.py
+++ gan2.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import savefig
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.ops import variable_scope
@@ -21,6 +22,20 @@ def sample_data(size, length=100):
     return np.array(data)
 
 
+def gen_samples(batch_num=100, batch_size=1000, sample_dim=100):
+    '''
+    :param sample_dim:产生样本的维度
+    样本必须固定，而不是在训练中每次重新产生
+    '''
+    print("begin to generate sample data")
+    samples = []
+    for i in range(batch_num):
+        sample = sample_data(batch_size, sample_dim)
+        samples.append(sample)
+    print("generating process finish")
+    return samples
+
+
 def random_data(size, length=100):
     """
     随机生成数据
@@ -31,52 +46,58 @@ def random_data(size, length=100):
     data = []
     for _ in range(size):
         # x = np.random.random(length) 
-        x = np.random.uniform(size=length)
+        x = np.random.uniform(-1, 1,length)
         data.append(x)
     return np.array(data)
 
 
-# 记录均值和均方差
-# x = tf.placeholder(tf.float32, shape=[None, 2], name="feature")  # [mean，std] -》 D
-# 样本的标签数据
-y_real = tf.placeholder(tf.float32, shape=[None, 1])
-y_fake = tf.placeholder(tf.float32, shape=[None, 1])
-# in_size = LENGTH
-# out_size = LENGTH
 
-input_real = tf.placeholder(tf.float32, shape=[None, dim])
+
+
 # 输入到Gen中的数据
 input_fake = tf.placeholder(tf.float32, shape=[None, dim])
+
+# 输入到Discrim中的数据
+input_real = tf.placeholder(tf.float32, shape=[None, dim])
 # 用Gen生成数据
 fake_data = Gen(input_fake)
 
+# Discrim的输出
 output_real_, output_real = Discrim(input_real)
 output_fake_, output_fake = Discrim(fake_data)
 
+# 样本的标签数据
+y_real = tf.placeholder(tf.float32, shape=[None, 1])
+y_fake = tf.placeholder(tf.float32, shape=[None, 1])
+
+# 产生用于训练的样本数据
+sample_datas = gen_samples(100, 1000, dim)
 
 
 ##################################### 定义损失函数
 # D的损失函数
 d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output_real_, labels=y_real))
 d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output_fake_, labels=y_fake))
-d_loss = (d_loss_real + d_loss_fake)/2 # 二分类交叉熵
+d_loss = d_loss_real + d_loss_fake
 # G的损失函数
-g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output_fake_, labels=y_fake))  # GAN二分类交叉熵
+g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output_fake_, labels=y_fake))  
 
 #################################### 定义优化器
-# G的优化器
-g_optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(
-    g_loss,
-    global_step=tf.Variable(0),
-    var_list=[t for t in tf.global_variables() if t.name.startswith('Generator')]
-)
-
 # D的优化器
 d_optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(
     d_loss,
-    global_step=tf.Variable(0),
+    # global_step=tf.Variable(0),
     var_list=[t for t in tf.trainable_variables() if t.name.startswith('Discrim')]
 )
+
+# G的优化器
+g_optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(
+    g_loss,
+    # global_step=tf.Variable(0),
+    var_list=[t for t in tf.global_variables() if t.name.startswith('Generator')]
+)
+
+
 
 d_loss_history = []
 g_loss_history = []
@@ -85,14 +106,18 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     # GAN博弈开始
     print('train GAN....')
+    test = sample_datas[0]
+    print("train data mean is", test.mean(), "its std is", test.std())
+    # (d, b) = np.histogram(sample_datas[0])
+    # plt.plot(b[:-1], d, c="y")
     for step in range(epoch):
         # batchsize为1000,数据集大小为100 * 1000
-        for _ in range(100):
+        for indx in range(100):
             # 使用G生成一批样本:
-            real = sample_data(1000,length=dim)
+            real = sample_datas[indx]
             noise = random_data(1000,length=dim)
 
-            # 训练判别网络
+            # 训练D
             d_loss_value, _ = sess.run([d_loss, d_optimizer], feed_dict={
                 input_real:real,
                 input_fake:noise,
@@ -101,30 +126,38 @@ with tf.Session() as sess:
             })  
             # 记录数据，用于绘图
             d_loss_history.append(d_loss_value)
-        # 将参数移动过去GAN中的判别网络
-        # dp_value = sess.run(D_PARAMS)
-        # dp_value = sess.run([t for t in tf.global_variables() if t.name.startswith('Discrim')])
-        # for i, v in enumerate([t for t in tf.global_variables() if t.name.startswith('GAN')]):
-        #     sess.run(v.assign(dp_value[i]))
 
         for _ in range(100):
             noise = random_data(100,length=dim)
             # 调整G，让GAN的误差减少
             g_loss_value, _ = sess.run([g_loss, g_optimizer], feed_dict={
                 input_fake: noise,
-                y_fake: np.zeros((len(noise),1)) # 混肴为目标,不需要加入x，我们只是借助G，并不需要训练G
-                # y:tf.ones(len(noise))
+                y_fake: np.ones((len(noise),1)) 
             })  
             g_loss_history.append(g_loss_value)
 
-        if step % 20 == 0 or step+1 == epoch:
-            noise = random_data(1,length=dim)
-            generate = sess.run(fake_data, feed_dict={
+        for _ in range(100):
+            noise = random_data(100,length=dim)
+            # 调整G，让GAN的误差减少
+            g_loss_value, _ = sess.run([g_loss, g_optimizer], feed_dict={
                 input_fake: noise,
-                y_fake: np.zeros((len(noise),1))
-            })
-            print("[%4d] GAN-d-loss: %.12f  GAN-g-loss: %.12f   generate-mean: %.4f   generate-std: %.4f" % (step,
-                            d_loss_value, g_loss_value, generate.mean(), generate.std() ))
+                y_fake: np.ones((len(noise),1)) 
+            })  
+            g_loss_history.append(g_loss_value)
+
+        noise = random_data(1,length=dim)
+        generate = sess.run(fake_data, feed_dict={
+            input_fake: noise,
+            y_fake: np.ones((len(noise),1))
+        })
+        print("[%4d] GAN-d-loss: %.12f  GAN-g-loss: %.12f   generate-mean: %.4f   generate-std: %.4f" % (step,
+                        d_loss_value, g_loss_value, generate.mean(), generate.std() ))
+        (data, bins) = np.histogram(generate[0])
+        (test, bins2) = np.histogram(noise[0])
+        plt.plot(bins[:-1], data, c="r")
+        plt.plot(bins2[:-1], test, c='b')
+        savefig('epoch' + str(step)+".jpg")
+
             
             
     print("train finish...")
@@ -133,7 +166,7 @@ with tf.Session() as sess:
 # plt.plot(d_loss_history)
 # a = plt.subplot(212)
 # plt.plot(g_loss_history,c="g")
-print("draw pic")
+
 real = sample_data(1,length=dim)
 (data, bins) = np.histogram(real[0])
 plt.plot(bins[:-1], data, c="g")
@@ -141,20 +174,3 @@ plt.plot(bins[:-1], data, c="g")
 
 (data, bins) = np.histogram(noise[0])
 plt.plot(bins[:-1], data, c="b")
-
-print("draw finished")
-
-# with tf.Session() as sess:
-#     sess.run(tf.global_variables_initializer())
-#     generate = sess.run(G_output3, feed_dict={
-#             z: noise
-#     })
-(data, bins) = np.histogram(generate[0])
-plt.plot(bins[:-1], data, c="r")
-
-# #x - x * z + log(1 + exp(-x))
-
-# pre = np.array([1,0])
-# real = np.array([0,1])
-
-# pre-pre*real + np.log(1+np.exp(-pre))
