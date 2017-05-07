6c45fd95dd2692c417afe0b7477c8f34b5116dc3
mxl1990 <mxl1990@gmail.com>
2017-05-07 23:22:39 +0800

    去除多余的初始化部分

--- discriminator.py
+++ discriminator.py
@@ -8,18 +8,18 @@ def Discrim(input_data):
     reuse = len([t for t in tf.global_variables() if t.name.startswith('Discrim')]) > 0
     with variable_scope.variable_scope('Discrim', reuse = reuse):
         dWeights = tf.Variable(tf.random_normal([dim, 32]))
-        dbiases = tf.Variable(tf.zeros([1, 32]) + 0.1)
+        dbiases = tf.Variable(tf.zeros([1, 32]))
         D_output = tf.matmul(input_data, dWeights) + dbiases
         D_output = tf.nn.relu(D_output)
         # 第二层
         dWeights2 = tf.Variable(tf.random_normal([32, 32]))
-        dbiases2 = tf.Variable(tf.zeros([1, 32]) + 0.1)
+        dbiases2 = tf.Variable(tf.zeros([1, 32]))
         D_output2 = tf.matmul(D_output, dWeights2) + dbiases2
         D_output2 = tf.nn.sigmoid(D_output2)
 
         # 第三层
         dWeights3 = tf.Variable(tf.random_normal([32, 1]))
-        dbiases3 = tf.Variable(tf.zeros([1, 1]) + 0.1)
+        dbiases3 = tf.Variable(tf.zeros([1, 1]))
         D_output3_ = tf.matmul(D_output2, dWeights3) + dbiases3
         D_output3 = tf.nn.sigmoid(D_output3_)
 
