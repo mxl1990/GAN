ea08e53a17cccc39852cf0560d30585c2b449b53
mxl1990 <mxl1990@gmail.com>
2017-05-17 21:08:11 +0800

    1.修改G的部分网络结构 2.修改部分参数的初始化

--- generator.py
+++ generator.py
@@ -2,17 +2,17 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 from tensorflow.python.ops import variable_scope
+# from gan2 import dim
 dim = 1000
 
 def Gen(noise):
 	###################################### G  网络结构
 	# 第一层
 	# 每个噪声数据维度为LENGTH
-	reuse = len([t for t in tf.global_variables() if t.name.startswith('Generator')]) > 0
-	with variable_scope.variable_scope('Generator', reuse = reuse):
+	with tf.variable_scope('Generator') as scope:
 		# z = tf.placeholder(tf.float32, shape=[None, LENGTH])  # 随机值噪音
 		# 随机权重
-		Weights = tf.Variable(tf.random_normal([dim, 32]), name='dw1')
+		Weights = tf.Variable(tf.random_uniform([dim, 32], -1, 1), name='dw1')
 		# 偏差为0.1
 		biases = tf.Variable(tf.constant(0.1, shape = [1, 32]), name='db1')
 		# G_output = z * w + b
@@ -24,7 +24,7 @@ def Gen(noise):
 		tf.summary.histogram("dw1_gen", Weights)
 		tf.summary.histogram("db1_gen", biases)
 		# 第二层
-		Weights2 = tf.Variable(tf.random_normal([32, 32]), name='dw2')
+		Weights2 = tf.Variable(tf.random_uniform([32, 32], -1, 1), name='dw2')
 		biases2 = tf.Variable(tf.constant(0.1, shape = [32]), name='db2')
 		G_output2 = tf.matmul(G_output, Weights2) + biases2
 
@@ -33,9 +33,10 @@ def Gen(noise):
 		# 第二层激活函数为sigmoid
 		G_output2 = tf.nn.sigmoid(G_output2)
 		# 第三层
-		Weights3 = tf.Variable(tf.random_normal([32, dim]), name='dw3')
+		Weights3 = tf.Variable(tf.random_uniform([32, dim], -1, 1), name='dw3')
 		biases3 = tf.Variable(tf.constant(0.1, shape = [dim]), name='db3')
 		G_output3 = tf.matmul(G_output2, Weights3) + biases3
+		G_output3 = tf.nn.relu(G_output3)
 
 		tf.summary.histogram("dw3_gen", Weights3)
 		tf.summary.histogram("db3_gen", biases3)
