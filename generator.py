0b8e2130206b7ea8d7a4ad29402459ffef2dd62a
mxl1990 <mxl1990@gmail.com>
2017-05-23 20:54:11 +0800

    1.修改G的网络结构

--- generator.py
+++ generator.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import tensorflow.contrib.slim as slim
 from tensorflow.python.ops import variable_scope
 # from gan2 import dim
-dim = 1000
+from globals_var import dim
 
 def Gen(noise):
 	###################################### G  网络结构
@@ -19,7 +19,8 @@ def Gen(noise):
 		G_output = tf.matmul(noise, Weights) + biases
 		# Rectified Linear Units激活函数
 		# relu(x) = max(0,x)即比0大就取本身
-		G_output = tf.nn.relu(G_output)
+		# G_output = tf.nn.relu(G_output)
+		G_output = tf.nn.sigmoid(G_output)
 
 		tf.summary.histogram("dw1_gen", Weights)
 		tf.summary.histogram("db1_gen", biases)
@@ -31,12 +32,13 @@ def Gen(noise):
 		tf.summary.histogram("dw2_gen", Weights2)
 		tf.summary.histogram("db2_gen", biases2)
 		# 第二层激活函数为sigmoid
-		G_output2 = tf.nn.sigmoid(G_output2)
+		G_output2 = tf.nn.relu(G_output2)
 		# 第三层
 		Weights3 = tf.Variable(tf.random_uniform([32, dim], -1, 1), name='dw3')
 		biases3 = tf.Variable(tf.constant(0.1, shape = [dim]), name='db3')
-		G_output3 = tf.matmul(G_output2, Weights3) + biases3
-		G_output3 = tf.nn.relu(G_output3)
+		G_output3= tf.matmul(G_output2, Weights3) + biases3
+		# G_output3 = tf.nn.relu(G_output3)
+		# G_output3 = tf.nn.sigmoid(G_output3)
 
 		tf.summary.histogram("dw3_gen", Weights3)
 		tf.summary.histogram("db3_gen", biases3)
