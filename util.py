626851528e69e830bff98a675c83f73a82b4298d
mxl1990 <mxl1990@gmail.com>
2017-07-28 20:02:10 +0800

    1.move the image normalization algorithm into this file 2.add merge images function, but still have some problem

--- util.py
+++ util.py
@@ -48,7 +48,7 @@ def random_data(size, length=100):
 	data = []
 	for _ in range(size):
 		# x = np.random.random(length) 
-		x = np.random.uniform(-1, 1,length)
+		x = np.random.uniform(-1.0, 1.0,length)
 		data.append(x)
 	return np.array(data)
 
@@ -64,3 +64,43 @@ def gen_noises(batch_num=100, batch_size=1000, data_dim=100):
 		sample = random_data(batch_size, data_dim)
 		noises.append(sample)
 	return noises
+
+def normalize_image(images):
+	# print(images)
+	return images / 127.5 - 1
+
+def denormal_image(images):
+	return (images + 1)/ 2
+
+
+def save_images(images, size, image_path):
+  return imsave(images, size, image_path)
+
+
+def imsave(images, size, path):
+  image = np.squeeze(merge(images, size))
+  from scipy.misc import imsave
+  return imsave(path, image)
+
+
+def merge(images, size): # images是图片列表，size是图片个数
+  h, w = images.shape[1], images.shape[2]
+  if (images.shape[3] in (3,4)):
+    c = images.shape[3]
+    img = np.zeros((h * size[0], w * size[1], c))
+    for idx, image in enumerate(images):
+      i = idx % size[1]
+      j = idx // size[1]
+      img[j * h:j * h + h, i * w:i * w + w, :] = image
+    return img
+  elif images.shape[3]==1:
+    img = np.zeros((h * size[0], w * size[1]))
+    for idx, image in enumerate(images):
+      i = idx % size[1]
+      j = idx // size[1]
+      img[j * h:j * h + h, i * w:i * w + w] = image[:,:,0]
+    return img
+  else:
+    raise ValueError('in merge(images,size) images parameter '
+                     'must have dimensions: H x W or H x W x 3 or H x W x 4')
+
