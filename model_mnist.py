ad93544641dcb0a8fa9405326ec69ad38f823ffc
mxl1990 <mxl1990@gmail.com>
2017-08-02 19:58:40 +0800

    1.use tf.zeros to initialize biase in layer, and set them to zero 2.delete max-out layer temporarily 3.use data from jpg not tensorflow Finally, current model can produce meaningful result

--- model_mnist.py
+++ model_mnist.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 from tensorflow.python import debug as tfdbg
 import numpy as np
-from util import normalize_image, denormal_image, random_data, save_images
+from util import normalize_image, denormal_image, random_data# , # save_images
 from scipy.misc import imsave
 
 
@@ -49,30 +49,26 @@ class GAN(object):
 			gen_dim = self.gen_dim
 			input_dim = self.input_dim
 
-			Weights = tf.Variable(tf.random_uniform([input_dim, gen_dim], -0.1, 0.1), name='dw1')
-			biases = tf.Variable(0., [gen_dim], name='db1')
+			Weights = tf.Variable(tf.random_uniform([input_dim, gen_dim], -0.1, 0.1), name='gen_dw1')
+			biases = tf.Variable(tf.zeros([gen_dim]), name='db1')
 			G_output = tf.matmul(noise, Weights) + biases
 			# G_output = tf.nn.relu(G_output)
-			G_output = tf.nn.relu(G_output)
-			# G_output = tf.nn.sigmoid(G_output)
+			G_output = tf.nn.tanh(G_output)
 
 			tf.summary.histogram("dw1_gen", Weights)
 			tf.summary.histogram("db1_gen", biases)
 
-			# 第二层
-			Weights2 = tf.Variable(tf.random_uniform([gen_dim, gen_dim], -0.1, 0.1), name='dw2')
-			biases2 = tf.Variable(0., [gen_dim], name='db2')
+			Weights2 = tf.Variable(tf.random_uniform([gen_dim, gen_dim], -0.1, 0.1), name='gen_dw2')
+			biases2 = tf.Variable(tf.zeros([gen_dim]), name='db2')
 			G_output2 = tf.matmul(G_output, Weights2) + biases2
-			# 第二层激活函数为sigmoid
 			# G_output2 = tf.nn.relu(G_output2)
-			G_output2 = tf.nn.relu(G_output2)
+			G_output2 = tf.nn.tanh(G_output2)
 
 			tf.summary.histogram("dw2_gen", Weights2)
 			tf.summary.histogram("db2_gen", biases2)
 			
-			# 第三层
-			Weights3 = tf.Variable(tf.random_uniform([gen_dim, input_dim], -0.1, 0.1), name='dw3')
-			biases3 = tf.Variable(0., [input_dim], name='db3')
+			Weights3 = tf.Variable(tf.random_uniform([gen_dim, input_dim], -0.1, 0.1), name='gen_dw3')
+			biases3 = tf.Variable(tf.zeros([input_dim]), name='db3')
 			G_output3= tf.matmul(G_output2, Weights3) + biases3
 			G_output3_ = tf.nn.sigmoid(G_output3)
 
@@ -84,60 +80,91 @@ class GAN(object):
 	def discriminator(self, input_data, reuse=False):
 		input_dim = self.input_dim
 		dis_dim = self.dis_dim
-		sess = self.sess
 		with tf.variable_scope("Discrim") as scope:
 			if reuse:
 				scope.reuse_variables()
 
-			dWeights = tf.get_variable(name='dw1', shape=[input_dim, dis_dim, 5], 
+			dWeights = tf.get_variable(name='dis_w1', shape=[input_dim, dis_dim], 
 							initializer=tf.random_uniform_initializer(-0.1, 0.1))
-			dbiases = tf.get_variable(name='db1', shape = [dis_dim, 5], 
+			dbiases = tf.get_variable(name='dis_b1', shape = [dis_dim], 
 							initializer=tf.constant_initializer(0.))
 
-			D_output = tf.tensordot(input_data, dWeights, axes=1) + dbiases
-			D_output = tf.reduce_max(D_output, axis=2)
+			D_output = tf.matmul(input_data, dWeights) + dbiases
+			D_output = tf.nn.tanh(D_output)
 			D_output = tf.nn.dropout(D_output, self.drop_possible)
-			# 用于可视化
 			tf.summary.histogram("dw1", dWeights)
 			tf.summary.histogram("db1", dbiases)
 
-			# 第二层
-			dWeights2 = tf.get_variable(name='dw2', shape=[dis_dim, dis_dim, 5], 
+			dWeights2 = tf.get_variable(name='dis_w2', shape=[dis_dim, dis_dim], 
 							initializer=tf.random_uniform_initializer(-0.1, 0.1))
-			dbiases2 = tf.get_variable(name='db2', shape = [dis_dim, 5], 
+			dbiases2 = tf.get_variable(name='dis_b2', shape = [dis_dim], 
 							initializer=tf.constant_initializer(0.))
 
-			D_output2 = tf.tensordot(D_output, dWeights2, axes=1) + dbiases2
-			D_output2 = tf.reduce_max(D_output2, axis=2)
+			D_output2 = tf.matmul(D_output, dWeights2) + dbiases2
+			D_output2 = tf.nn.tanh(D_output2)
 			D_output2 = tf.nn.dropout(D_output2, self.drop_possible)
-
 			tf.summary.histogram("dw2", dWeights2)
 			tf.summary.histogram("db2", dbiases2)
 
-			# 第三层
-			dWeights3 = tf.get_variable(name='dw3', shape=[dis_dim, 1], 
+			dWeights3 = tf.get_variable(name='dis_w3', shape=[dis_dim, 1], 
 							initializer=tf.random_uniform_initializer(-0.1, 0.1))
-			dbiases3 = tf.get_variable(name='db3', shape = [1], 
+			dbiases3 = tf.get_variable(name='dis_b3', shape = [1], 
 							initializer=tf.constant_initializer(0.))
 
 			D_output3 = tf.matmul(D_output2, dWeights3) + dbiases3
 			D_output3_ = tf.nn.sigmoid(D_output3)
-
 			tf.summary.histogram("dw3", dWeights3)
 			tf.summary.histogram("db3", dbiases3)
 
-			return D_output3, D_output3_
+			# 返回的后者是sigmoid的结果
+			return D_output3, D_output3_			
+
+
+			# dWeights = tf.get_variable(name='dw1', shape=[input_dim, dis_dim, 5], 
+			# 				initializer=tf.random_uniform_initializer(-0.1, 0.1))
+			# dbiases = tf.get_variable(name='db1', shape = [dis_dim, 5], 
+			# 				initializer=tf.constant_initializer(0.))
+
+			# D_output = tf.tensordot(input_data, dWeights, axes=1) + dbiases
+			# D_output = tf.reduce_max(D_output, axis=2)
+			# D_output = tf.nn.tanh(D_output)
+			# D_output = tf.nn.dropout(D_output, self.drop_possible)
+			# tf.summary.histogram("dw1", dWeights)
+			# tf.summary.histogram("db1", dbiases)
+
+			# dWeights2 = tf.get_variable(name='dw2', shape=[dis_dim, dis_dim, 5], 
+			# 				initializer=tf.random_uniform_initializer(-0.1, 0.1))
+			# dbiases2 = tf.get_variable(name='db2', shape = [dis_dim, 5], 
+			# 				initializer=tf.constant_initializer(0.))
+
+			# D_output2 = tf.tensordot(D_output, dWeights2, axes=1) + dbiases2
+			# D_output2 = tf.reduce_max(D_output2, axis=2)
+			# D_output2 = tf.nn.tanh(D_output2)
+			# D_output2 = tf.nn.dropout(D_output2, self.drop_possible)
+
+			# tf.summary.histogram("dw2", dWeights2)
+			# tf.summary.histogram("db2", dbiases2)
+
+			# dWeights3 = tf.get_variable(name='dw3', shape=[dis_dim, 1], 
+			# 				initializer=tf.random_uniform_initializer(-0.1, 0.1))
+			# dbiases3 = tf.get_variable(name='db3', shape = [1], 
+			# 				initializer=tf.constant_initializer(0.))
+
+			# D_output3 = tf.matmul(D_output2, dWeights3) + dbiases3
+			# D_output3_ = tf.nn.sigmoid(D_output3)
+
+			# tf.summary.histogram("dw3", dWeights3)
+			# tf.summary.histogram("db3", dbiases3)
+
+			
 
 	def train(self, config):
 		#################################### 定义优化器
 		# D的优化器
-		# d_optimizer = tf.train.AdamOptimizer(0.0001).minimize(
-		global_step = tf.Variable(0)
-
 		with tf.name_scope('D_train'):
 			d_optimizer = tf.train.MomentumOptimizer(self.d_learn_rate, 0.5).minimize(
 				self.d_loss,
-				global_step=global_step,
+				# global_step=global_step,
 				var_list=[t for t in tf.global_variables() if t.name.startswith('Discrim')]
 			)
 
@@ -154,17 +181,33 @@ class GAN(object):
 		tf.global_variables_initializer().run()
 		sum_var = tf.summary.merge_all()
 
-		from tensorflow.examples.tutorials.mnist import input_data
-		data = input_data.read_data_sets(config.data_dir) # 载入训练数据
-		data = data.train # 仅保留训练数据
+		# from tensorflow.examples.tutorials.mnist import input_data
+		# data = input_data.read_data_sets(config.data_dir) # 载入训练数据
+		# data = data.train # 仅保留训练数据
 
 		writer = tf.summary.FileWriter(".//test", self.sess.graph)
 		writer.add_graph(self.sess.graph)
 		batch_size = config.batch_size
-		batch_num = data.num_examples // batch_size
 		learn_rate = config.learn_rate
-		# lr = tf.train.exponential_decay(learn_rate, global_step, 10000, 1.000004, name="learn_rate")
 		sess = self.sess
+
+		from glob import glob
+		import os
+		data = glob(os.path.join(config.data_dir, "*.jpg"))
+		images = []
+		from PIL import Image
+		for datum in data:
+			image = np.array(Image.open(datum), 'f')
+			image = np.resize(image, (self.input_dim))
+			images.append(image)
+
+		
+		images = np.array(images)
+		pic_num = len(images)
+		print("get %d images"%pic_num)
+		images = normalize_image(images)
+		# batch_num = pic_num // batch_size
+		batch_num = 1
 
 		import os
 		if not os.path.exists("./checkpoint"): # 创建checkpoint存放文件夹
@@ -174,17 +217,17 @@ class GAN(object):
 		print('train GAN....')
 		for step in range(config.epoch):
 			# 使用G生成一批样本:
-			# noises = gen_noises(100, 1000,data_dim=dim)
 			d_loss_sum = 0.0
 			g_loss_sum = 0.0
 
-			for batch in range(1):
-				# l_r = sess.run(lr)
-				# l_r = 0.000001 if l_r < .000001 else l_r
-				batch_data = normalize_image(data.next_batch(batch_size)[0])
-
-				noise = random_data(batch_size, self.input_dim)
+			for batch in range(batch_num):
+				# batch_data = normalize_image(data.next_batch(batch_size)[0])
+				
+				batch_data = images[batch: batch + batch_size]
+				# batch_data = images[0:batch_size]
+				
 				# 训练D
+				noise = random_data(batch_size, self.input_dim)
 				d_loss_value, _, sum_v= sess.run([self.d_loss, d_optimizer,sum_var], 
 					feed_dict={
 					self.input_real:batch_data,
@@ -192,7 +235,6 @@ class GAN(object):
 					self.d_learn_rate:learn_rate,
 					self.drop_possible: 0.5,
 				})  
-				# 记录数据，用于绘图
 				d_loss_sum = d_loss_sum + d_loss_value
 				writer.add_summary(sum_v, (step) * batch_num + batch)
 
@@ -211,29 +253,26 @@ class GAN(object):
 			})
 			generate = denormal_image(generate)
 			
-			# if step % 10 == 0:
-			print("[%4d] GAN-d-loss: %.12f  GAN-g-loss: %.12f" % (step, d_loss_sum / batch_num, g_loss_sum / batch_num))
 			if step % 100 == 0:
+				print("[%4d] GAN-d-loss: %.12f  GAN-g-loss: %.12f" % (step, d_loss_sum / batch_num, g_loss_sum / batch_num))
+				# idx = random.randint(0,batch_size)
+				generate = np.resize(generate[0],(28,28))
+				imsave("train%d_%d.jpg"%(step,0), generate)
 
-				# generate = generate[random.randint(0, batch_size-1)]
-				# generate = np.resize(generate, (28,28))
-				generate = generate[0] # 取64张图组合
-				generate = np.resize(generate,(28, 28))
-				# save_images(generate, (8,8), "./samples/train_%d.jpg"%step)
-				from scipy.misc import imsave
-				imsave("./samples/train_%d.jpg"%step,generate)
 			if step % 5000 == 0:
 				saver.save(self.sess, os.path.join("./checkpoint", 'gan.ckpt'))
 				print("check point saving...")
+
+			# if step % 10000 == 0:
+			# 	learn_rate = learn_rate / 10
 
 		noise = random_data(batch_size, self.input_dim)
 		generate = sess.run(self.fake_data, feed_dict={
 				self.input_fake: noise,
 			})
-		generate = denormal_image(generate[0:65])
-		generate = np.resize(generate, (28*8,28*8))
+		generate = denormal_image(np.resize(generate[0],(28,28)))
 		imsave("final.jpg", generate)
-		print("train finish...")
+		print("train finished" + "."*10)
 
 	
 
