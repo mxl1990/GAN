c925a828c762e1fd22b44150f3fa854f10b11860
mxl1990 <mxl1990@gmail.com>
2017-08-17 20:20:31 +0800

    1.delete redundancy code 2.change some initial parameter

--- model_mnist.py
+++ model_mnist.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 from tensorflow.python import debug as tfdbg
 import numpy as np
-from util import normalize_image, denormal_image, random_data# , # save_images
+from util import normalize_image, denormal_image, random_data, save_images
 from scipy.misc import imsave
 
 
@@ -18,6 +18,7 @@ class GAN(object):
 		self.build_model()
 
 	def build_model(self):
+		print("begin to build GAN model")
 		self.input_fake = tf.placeholder(tf.float32, shape=[None, self.input_dim], name='input_noise')
 		# 输入到Discrim中的数据
 		self.input_real = tf.placeholder(tf.float32, shape=[None, self.input_dim], name='input_real')
@@ -43,13 +44,14 @@ class GAN(object):
 		with tf.name_scope('g_loss'):		
 			self.g_loss = -tf.reduce_mean(tf.log(self.output_fake))
 			tf.summary.scalar('g_loss_value', self.g_loss)
+		print("model building has finished")
 
 	def generator(self, noise):
 		with tf.variable_scope('Generator') as scope:
 			gen_dim = self.gen_dim
 			input_dim = self.input_dim
 
-			Weights = tf.Variable(tf.random_uniform([input_dim, gen_dim], -0.1, 0.1), name='gen_dw1')
+			Weights = tf.Variable(tf.random_uniform([input_dim, gen_dim], -0.05, 0.05), name='gen_dw1')
 			biases = tf.Variable(tf.zeros([gen_dim]), name='db1')
 			G_output = tf.matmul(noise, Weights) + biases
 			G_output = tf.nn.relu(G_output)
@@ -58,7 +60,7 @@ class GAN(object):
 			tf.summary.histogram("dw1_gen", Weights)
 			tf.summary.histogram("db1_gen", biases)
 
-			Weights2 = tf.Variable(tf.random_uniform([gen_dim, gen_dim], -0.1, 0.1), name='gen_dw2')
+			Weights2 = tf.Variable(tf.random_uniform([gen_dim, gen_dim], -0.05, 0.05), name='gen_dw2')
 			biases2 = tf.Variable(tf.zeros([gen_dim]), name='db2')
 			G_output2 = tf.matmul(G_output, Weights2) + biases2
 			G_output2 = tf.nn.relu(G_output2)
@@ -67,7 +69,7 @@ class GAN(object):
 			tf.summary.histogram("dw2_gen", Weights2)
 			tf.summary.histogram("db2_gen", biases2)
 			
-			Weights3 = tf.Variable(tf.random_uniform([gen_dim, input_dim], -0.1, 0.1), name='gen_dw3')
+			Weights3 = tf.Variable(tf.random_uniform([gen_dim, input_dim], -0.05, 0.05), name='gen_dw3')
 			biases3 = tf.Variable(tf.zeros([input_dim]), name='db3')
 			G_output3= tf.matmul(G_output2, Weights3) + biases3
 			G_output3_ = tf.nn.sigmoid(G_output3)
@@ -85,7 +87,7 @@ class GAN(object):
 				scope.reuse_variables()
 
 			dWeights = tf.get_variable(name='dw1', shape=[input_dim, dis_dim, 5], 
-							initializer=tf.random_uniform_initializer(-0.1, 0.1))
+							initializer=tf.random_uniform_initializer(-0.005, 0.005))
 			dbiases = tf.get_variable(name='db1', shape = [dis_dim, 5], 
 							initializer=tf.constant_initializer(0.))
 			D_output = tf.tensordot(input_data, dWeights, axes=1) + dbiases
@@ -96,7 +98,7 @@ class GAN(object):
 			tf.summary.histogram("db1", dbiases)
 
 			dWeights2 = tf.get_variable(name='dw2', shape=[dis_dim, dis_dim, 5], 
-							initializer=tf.random_uniform_initializer(-0.1, 0.1))
+							initializer=tf.random_uniform_initializer(-0.005, 0.005))
 			dbiases2 = tf.get_variable(name='db2', shape = [dis_dim, 5], 
 							initializer=tf.constant_initializer(0.))
 			D_output2 = tf.tensordot(D_output, dWeights2, axes=1) + dbiases2
@@ -107,7 +109,7 @@ class GAN(object):
 			tf.summary.histogram("db2", dbiases2)
 
 			dWeights3 = tf.get_variable(name='dw3', shape=[dis_dim, 1], 
-							initializer=tf.random_uniform_initializer(-0.1, 0.1))
+							initializer=tf.random_uniform_initializer(-0.005, 0.005))
 			dbiases3 = tf.get_variable(name='db3', shape = [1], 
 							initializer=tf.constant_initializer(0.))
 			D_output3 = tf.matmul(D_output2, dWeights3) + dbiases3
@@ -120,6 +122,9 @@ class GAN(object):
 			
 
 	def train(self, config):
+		batch_size = config.batch_size
+		learn_rate = config.learning_rate
+
 		#################################### 定义优化器
 		# D的优化器
 		with tf.name_scope('D_train'):
@@ -137,54 +142,43 @@ class GAN(object):
 				var_list=[t for t in tf.global_variables() if t.name.startswith('Generator')]
 			)
 
-
-		saver = tf.train.Saver()
-		tf.global_variables_initializer().run()
-		sum_var = tf.summary.merge_all()
-
-		from tensorflow.examples.tutorials.mnist import input_data
-		data = input_data.read_data_sets(config.data_dir, validation_size=0) # 载入训练数据，不需要验证数据
-		data = data.train # 仅保留训练数据
-
-		writer = tf.summary.FileWriter(".//test", self.sess.graph)
-		writer.add_graph(self.sess.graph)
-		batch_size = config.batch_size
-		learn_rate = config.learn_rate
-		sess = self.sess
-
-		# from glob import glob
-		# import os
-		# data = glob(os.path.join(config.data_dir, "*.jpg"))
-		# images = []
-		# from PIL import Image
-		# for datum in data:
-		# 	image = np.array(Image.open(datum), 'f')
-		# 	image = np.resize(image, (self.input_dim))
-		# 	images.append(image)
-
-		
-		# images = np.array(images)
-		# pic_num = len(images)
-		# print("get %d images"%pic_num)
-		# images = normalize_image(images)
-		# # batch_num = pic_num // batch_size
-		batch_num = 1
-		# batch_num = data.num_examples // batch_size
-
 		import os
 		if not os.path.exists("./checkpoint"): # 创建checkpoint存放文件夹
 			os.mkdir("./checkpoint")
-		import random
+		if not os.path.exists("./samples"): # 创建样本产生的文件夹
+			os.mkdir("./samples")
 
-		print('train GAN....')
+		writer = tf.summary.FileWriter(".//test", self.sess.graph)
+		writer.add_graph(self.sess.graph)
+		sum_var = tf.summary.merge_all()
+
+		saver = tf.train.Saver()
+		tf.global_variables_initializer().run()
+		
+
+		print("begin to get trainning data of MNIST")
+		from tensorflow.examples.tutorials.mnist import input_data
+		data = input_data.read_data_sets(config.datadir, validation_size=0) # 载入训练数据，不需要验证数据
+		data = data.train # 仅保留训练数据
+		batch_num = data.num_examples // batch_size
+		images = []
+		for i in range(batch_num):
+			images.append(data.next_batch(batch_size)[0])
+		print("data has prepared")
+
+		sess = self.sess
+		index = 0
+
+		print('begin to train GAN....')
 		for step in range(config.epoch):
 			# 使用G生成一批样本:
 			d_loss_sum = 0.0
 			g_loss_sum = 0.0
 
-			for batch in range(batch_num):
-				batch_data = data.next_batch(batch_size)[0]
-				# batch_data = images[batch: batch + batch_size]
+			for batch in range(1):
+
+				batch_data = images[index]
+				index = (index + 1) % batch_num
 				
 				# 训练D
 				noise = random_data(batch_size, self.input_dim)
@@ -211,12 +205,25 @@ class GAN(object):
 			generate = sess.run(self.fake_data, feed_dict={
 				self.input_fake: noise,
 			})
-			generate = denormal_image(generate)
+			# print("before generate0",generate[0])
+			# print("before generate1",generate[1])
+			# generate = denormal_image(generate)
+
+			if step == 5000:
+				print("generate0",generate[0])
+				print("generate1",generate[1])
+				image1 = np.resize(generate[0], (28,28))
+				image2 = np.resize(generate[1], (28,28))
+				imsave("./samples/5000_1.jpg", image1)
+				imsave("./samples/5000_2.jpg", image2)
 			
 			if step % 100 == 0:
-				print("[%4d] GAN-d-loss: %.12f  GAN-g-loss: %.12f" % (step, d_loss_sum / batch_num, g_loss_sum / batch_num))
-				generate = np.resize(generate[0],(28,28))
-				imsave("train%d_%d.jpg"%(step,0), generate)
+				print("[%4d] GAN-d-loss: %.12f  GAN-g-loss: %.12f" % (step, d_loss_sum / batch_num, g_loss_sum / batch_num))	
+				# generate = denormal_image(generate[0:64])
+				generate = generate[0:64]
+				# print("generate0",generate[0])
+				# print("generate1",generate[1])
+				save_images(generate, (8,8), (28,28,1), "./samples/train_%d.jpg"%step)
 
 			if step % 5000 == 0:
 				saver.save(self.sess, os.path.join("./checkpoint", 'gan.ckpt'))
@@ -226,8 +233,8 @@ class GAN(object):
 		generate = sess.run(self.fake_data, feed_dict={
 				self.input_fake: noise,
 			})
-		generate = denormal_image(np.resize(generate[0],(28,28)))
-		imsave("final.jpg", generate)
+		generate = denormal_image(generate[0:64])
+		save_images(generate, (8,8), (28,28,1), "./samples/final.jpg")
 		print("train finished" + "."*10)
 
 	
