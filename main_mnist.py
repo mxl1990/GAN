013fe66985007702dc50fab876da8e26cb30e8d4
mxl1990 <mxl1990@gmail.com>
2017-08-17 20:18:32 +0800

    1.add parameter to control the main function

--- main_mnist.py
+++ main_mnist.py
@@ -1,27 +1,33 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
 from model_mnist import GAN
-from util import empty
-from tensorflow.python import debug as tfdbg
 
-run_config = tf.ConfigProto()
-run_config.gpu_options.allow_growth=True
+FLAGS = tf.flags.FLAGS
 
-with tf.Session(config=run_config) as sess:
-	# sess = tfdbg.LocalCLIDebugWrapperSession(sess)
-	# sess.add_tensor_filter("has_inf_or_nan",tfdbg.has_inf_or_nan)
+tf.flags.DEFINE_integer('epoch', 10001,'The number of epoch, default:10001')
+tf.flags.DEFINE_integer('batch_size', 100,'The number of epoch, default:100')
 
-	gan = GAN(
-			sess,
-			input_dim = 28*28,
-			gen_layer_dim = 1200,
-			dis_layer_dim = 240
-			)
+tf.flags.DEFINE_float('learning_rate', 1e-2, 'learning rate, default: 0.01')
 
-	config = empty()
-	config.epoch = 10000
-	config.batch_size = 100
-	config.learn_rate = 0.01
-	config.data_dir = "./mnist_train"
+tf.flags.DEFINE_string('datadir', "./data/MNIST",'The dictionary of MNIST file put, defualt:./data/MNIST')
 
-	gan.train(config)
+def main(unused_argv):
+	run_config = tf.ConfigProto()
+	run_config.gpu_options.allow_growth=True
+
+	with tf.Session(config=run_config) as sess:
+		# from tensorflow.python import debug as tfdbg
+		# sess = tfdbg.LocalCLIDebugWrapperSession(sess)
+
+		gan = GAN(
+				sess,
+				input_dim = 28*28,
+				gen_layer_dim = 1200,
+				dis_layer_dim = 240
+				)
+
+		gan.train(FLAGS)
+
+
+if __name__ == '__main__':
+  tf.app.run()
